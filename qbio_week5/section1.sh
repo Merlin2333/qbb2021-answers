0a1e13764d54d126f5393bef1cc3118f5b3696e1
Merlin Li <mli163@jh.edu>
2021-12-10 11:38:11 -0500

    resubmit for regrade

--- qbio_week5/section1.sh
+++ qbio_week5/section1.sh
@@ -7,16 +7,16 @@
 
 #Align the sequence reads (unpaired) to the reference genome
 #TF and histone modification sites
-# bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/CTCF_ER4.fastq > CTCF_ER4_aln.sam
-# bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/CTCF_G1E.fastq > CTCF_G1E_aln.sam
+bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/CTCF_ER4.fastq > CTCF_ER4_aln.sam
+bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/CTCF_G1E.fastq > CTCF_G1E_aln.sam
 
 #Reference sequence sites aligned to the reference genome
-# bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/input_ER4.fastq > input_ER4_aln.sam
-# bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/input_G1E.fastq > input_G1E_aln.sam
+bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/input_ER4.fastq > input_ER4_aln.sam
+bowtie2 -x /Users/cmdb/data/indexes/bowtie2/mm10/mm10 -U /Users/cmdb/qbb2021-answers/qbio_week5/seq_reads/input_G1E.fastq > input_G1E_aln.sam
 
 #Calling peaks
-# macs2 callpeak -t CTCF_ER4_aln.sam -c input_ER4_aln.sam --name=ER4 --gsize=mm --outdir /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks --bdg
-# macs2 callpeak -t CTCF_G1E_aln.sam -c input_G1E_aln.sam --name=G1E --gsize=mm --outdir /Users/cmdb/qbb2021-answers/qbio_week5/G1E_peaks --bdg
+macs2 callpeak -t CTCF_ER4_aln.sam -c input_ER4_aln.sam --name=ER4 --gsize=mm --outdir /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks --bdg
+macs2 callpeak -t CTCF_G1E_aln.sam -c input_G1E_aln.sam --name=G1E --gsize=mm --outdir /Users/cmdb/qbb2021-answers/qbio_week5/G1E_peaks --bdg
 
 #Differential binding
 #Gain of CTCF binding E-G
@@ -24,9 +24,14 @@ bedtools subtract -A -a /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks/ER4_pea
 #Loss of CTCF binding G-E
 bedtools subtract -A -a /Users/cmdb/qbb2021-answers/qbio_week5/G1E_peaks/G1E_peaks.narrowPeak -b /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks/ER4_peaks.narrowPeak > loss_CTCF_binding.bed
 
+
 #Feature overlapping
 #E4 features overlap
 #bedtools intersect allows one to screen for overlaps between two sets of genomic features.
-# bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks/ER4_peaks.narrowPeak -wo > ER4_feature.bed
+bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b /Users/cmdb/qbb2021-answers/qbio_week5/ER4_peaks/ER4_peaks.narrowPeak -wo > ER4_feature.bed
 #G1E features overlap
-# bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b /Users/cmdb/qbb2021-answers/qbio_week5/G1E_peaks/G1E_peaks.narrowPeak -wo > G1E_feature.bed
+bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b /Users/cmdb/qbb2021-answers/qbio_week5/G1E_peaks/G1E_peaks.narrowPeak -wo > G1E_feature.bed
+
+#Feature genration for the sites that are either gained or lost after differentitaion
+bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b gain_CTCF_binding.bed > peaks_gained_features.bed
+bedtools intersect -a /Users/cmdb/qbb2021-answers/qbio_week5/Mus_musculus.GRCm38.94_features.bed -b loss_CTCF_binding.bed > peaks_lost_features.bed
